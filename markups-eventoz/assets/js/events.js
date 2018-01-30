d2ee56accef516c1cfa24df2a8d9a9d1628560c6
Ajayjam97 <jamwal_ajay@hotmail.com>
2018-01-31 00:02:34 +0530

    Carousel and some animation
    added code to event.js, carousle_style.css and index.html for carousel
    and animated header section.

--- markups-eventoz/assets/js/events.js
+++ markups-eventoz/assets/js/events.js
@@ -12,3 +12,14 @@ function eventpdf() {
 }
 
 
+$("#carousel > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#carousel > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#carousel');
+}, 3000);
+
