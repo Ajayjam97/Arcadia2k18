177e2d9b0d54eb98db54358c56b4bf15e6acb24c
RachitGupta008 <35948433+RachitGupta008@users.noreply.github.com>
2018-01-30 16:41:01 +0530

    this will handle the links for the pdfs for events

--- markups-eventoz/assets/js/events.js
+++ markups-eventoz/assets/js/events.js
@@ -1,1 +1,14 @@
+var eventobj = {
+'0' : '1.pdf',
+'1':'2.pdf',
+'2':'3.pdf'
+};
+var base_url = "file:///A:/Arcadia2k18-rdedit/markups-eventoz";
+
+function eventpdf() {
+	
+	var drop = document.getElementById('events').value;
+    document.getElementById('event_submit').innerHTML = '<a target="_blank" class="mu-reg-submit-btn"  href="'+base_url+'/'+eventobj[drop]+'">SUBMIT</a>';
+}
+
 
